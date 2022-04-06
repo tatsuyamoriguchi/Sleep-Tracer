99b995a8281ed7a4d3d29b65695a0cca9778d2ea
Brian Moriguchi <brian@beckos.com>
2022-04-06 10:30:19 -0700

    Initial Commit

---
+++ Sleep Tracer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sleep Tracer
+//
+//  Created by Tatsuya Moriguchi on 4/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sleep Tracer/Sleep_TracerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sleep_TracerApp.swift
+//  Sleep Tracer
+//
+//  Created by Tatsuya Moriguchi on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Sleep_TracerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
