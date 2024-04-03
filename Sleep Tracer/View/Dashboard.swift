18d0047d10cb9d322f0131c30d296db370ea0745
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-04-03 14:45:53 -0700

    Added Dashboard, Logout Tab items

---
+++ Sleep Tracer/View/Dashboard.swift
@@ -0,0 +1,18 @@
+//
+//  Dashboard.swift
+//  Sleep Tracer
+//
+//  Created by Tatsuya Moriguchi on 4/3/24.
+//
+
+import SwiftUI
+
+struct Dashboard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Dashboard()
+}
