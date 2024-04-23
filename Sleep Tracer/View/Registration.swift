ef0d0587a401e88f986dc2c420e9a0d4e7da04a1
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-04-22 20:19:43 -0700

    Refactoring TextField modifiers

---
+++ Sleep Tracer/View/Registration.swift
@@ -0,0 +1,18 @@
+//
+//  Registration.swift
+//  Sleep Tracer
+//
+//  Created by Tatsuya Moriguchi on 4/21/24.
+//
+
+import SwiftUI
+
+struct Registration: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Registration()
+}
---
+++ Sleep Tracer/View/TextFieldStyle.swift
@@ -0,0 +1,8 @@
+//
+//  TextFieldStyle.swift
+//  Sleep Tracer
+//
+//  Created by Tatsuya Moriguchi on 4/22/24.
+//
+
+import Foundation
